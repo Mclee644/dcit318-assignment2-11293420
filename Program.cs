6a840531c65d886c23d3ee13d141aea591be48a6
Mclee644 <644mcl@gmail.com>
2025-08-05 10:06:00 +0000

    implementing inheritance and method overriding

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program{
+	static void Main(string[] args){
+		string title = """
+
+		|	INHERITANCE AND METHOD OVERRIDING	|
+
+		""";
+
+		Console.WriteLine(title);
+		Animal onipa = new Animal();
+		onipa.MakeSound();
+
+		Dog adom = new Dog();
+		adom.MakeSound();
+
+		Cat puss = new Cat();
+		puss.MakeSound();
+	}
+}
+
+class Animal{
+	public virtual void MakeSound(){
+		Console.WriteLine("Some generic sound");
+	}
+}
+
+class Dog:Animal{
+	public override void MakeSound(){
+		Console.WriteLine("Bark");
+	}
+}
+
+class Cat:Animal{
+	public override void MakeSound(){
+		Console.WriteLine("Meow");
+	}
+}
